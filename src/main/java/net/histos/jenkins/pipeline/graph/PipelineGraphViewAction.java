a15fc2365fdb27d426d0d51c1188ac78f247dbab
Tim Jacomb <timjacomb1+github@gmail.com>
2021-02-23 10:30:55 +0000

    Add ICON

--- src/main/java/net/histos/jenkins/pipeline/graph/PipelineGraphViewAction.java
+++ src/main/java/net/histos/jenkins/pipeline/graph/PipelineGraphViewAction.java
@@ -1,12 +1,22 @@
 package net.histos.jenkins.pipeline.graph;
 
 import hudson.model.Action;
+import org.jenkins.ui.icon.Icon;
+import org.jenkins.ui.icon.IconSet;
+import org.jenkins.ui.icon.IconSpec;
 
-public class PipelineGraphViewAction implements Action {
+public class PipelineGraphViewAction implements Action, IconSpec {
+
+    static {
+        IconSet.icons.addIcon(new Icon("icon-pipeline-graph icon-md", "plugin/pipeline-graph-view/images/24x24/blueocean.png", Icon.ICON_MEDIUM_STYLE));
+        IconSet.icons.addIcon(new Icon("icon-pipeline-graph icon-xl", "plugin/pipeline-graph-view/images/48x48/blueocean.png", Icon.ICON_XLARGE_STYLE));
+    }
 
     @Override
     public String getIconFileName() {
-        return null;
+        String iconClassName = getIconClassName();
+        Icon icon = IconSet.icons.getIconByClassSpec(iconClassName + " icon-md");
+        return "/plugin/" + icon.getUrl();
     }
 
     @Override
@@ -18,5 +28,9 @@ public class PipelineGraphViewAction implements Action {
     public String getUrlName() {
         return "pipelineGraph";
     }
-    
+
+    @Override
+    public String getIconClassName() {
+        return "icon-pipeline-graph";
+    }
 }

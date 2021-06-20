6dc21374da2d9ec9bb4210db3493a6c3a0b6b791
timbrown5 <bittebrown@gmail.com>
2021-06-20 18:53:11 +0100

    CLeanup DataTreeView.tsx

--- src/main/frontend/pipeline-console-view/pipeline-console/main/DataTreeView.tsx
+++ src/main/frontend/pipeline-console-view/pipeline-console/main/DataTreeView.tsx
@@ -83,53 +83,36 @@ const getTreeItemsFromStepList = (stepsItems: StepInfo[]) => {
   });
 };
 
+const getStageTreeItem = (
+    childStages: StageInfo[],
+    childSteps: Map<String, StepInfo[]>
+  ) => {
+
+}
+
+
 const getTreeItemsFromStage = (
   stageItems: StageInfo[],
   stageSteps: Map<String, StepInfo[]>
 ) => {
-
-  // TODO: Refactor to make more DRY
   return stageItems.map((stageItemData) => {
+    let children : JSX.Element[] = []
     if (stageItemData.children && stageItemData.children.length > 0) {
-      let children = getTreeItemsFromStage(stageItemData.children, stageSteps);
-      let steps = stageSteps.get(`${stageItemData.id}`);
-      if (steps) {
-        console.log(`!!!!Found steps for ${stageItemData.id}, ${steps}`);
-        let stepsItems = getTreeItemsFromStepList(steps);
-        children = [...children, ...stepsItems];
-      } else {
-        console.log(`????Found no steps for ${stageItemData.id}`);
-      }
-      return (
-        <StageTreeItem
-          key={stageItemData.id}
-          nodeId={String(stageItemData.id)}
-          label={stageItemData.name}
-          children={children}
-        />
-      );
-    } else {
-      let steps = stageSteps.get(`${stageItemData.id}`);
-      if (steps) {
-        let stepsItems = getTreeItemsFromStepList(steps);
-        return (
-          <StageTreeItem
-            key={stageItemData.id}
-            nodeId={String(stageItemData.id)}
-            label={stageItemData.name}
-            children={stepsItems}
-          />
-        );
-      } else {
-        return (
-          <StageTreeItem
-            key={stageItemData.id}
-            nodeId={String(stageItemData.id)}
-            label={stageItemData.name}
-          />
-        );
-      }
+      children = [...getTreeItemsFromStage(stageItemData.children, stageSteps)]
     }
+    let steps = stageSteps.get(`${stageItemData.id}`);
+    if (steps) {
+      let stepsItems = getTreeItemsFromStepList(steps);
+      children = [...children, ...stepsItems]
+    }
+    return (
+      <StageTreeItem
+        key={stageItemData.id}
+        nodeId={String(stageItemData.id)}
+        label={stageItemData.name}
+        children={children}
+      />
+    );
   });
 };
 
@@ -192,11 +175,6 @@ export class DataTreeView extends React.Component {
       <TreeView
         defaultCollapseIcon={<ExpandMoreIcon />}
         defaultExpandIcon={<ChevronRightIcon />}
-        // Change so that onNodeSelect is a function which calls the 'stageActions'
-        // endpoint. That would return the list of actions for a Stage (so we only
-        // load actions that the user is interested in).
-        // Then we will pass 'this.props.onNodeSelect' into the Action `TreeItem`
-        // elements only (so stages do not update the console section).
         onNodeSelect={this.props.onActionNodeSelect}
       >
         {getTreeItemsFromStage(this.state.stages, this.state.steps)}

4667315c5b4d76f891495e9551b93bbdb6e0a4cb
timbrown5 <bittebrown@gmail.com>
2022-09-26 15:23:30 +0100

    Add function to scroll to element once it exists

--- src/main/frontend/pipeline-console-view/app.tsx
+++ src/main/frontend/pipeline-console-view/app.tsx
@@ -4,10 +4,35 @@ import { FunctionComponent } from "react";
 
 import { PipelineConsole } from "./pipeline-console/main/";
 
-//const rootElement = document.getElementById("root");
-//ReactDOM.render(<DataTreeView />, rootElement);
+const useScrollToLocation = () => {
+  const scrolledRef = React.useRef(false);
+  const hash = window.location.hash;
+  const hashRef = React.useRef(hash);
+
+  React.useEffect(() => {
+    if (hash) {
+      // We want to reset if the hash has changed
+      if (hashRef.current !== hash) {
+        hashRef.current = hash;
+        scrolledRef.current = false;
+      }
+
+      // only attempt to scroll if we haven't yet (this could have just reset above if hash changed)
+      if (!scrolledRef.current) {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          console.log("Found element, scrolling...")
+          element.scrollIntoView({ behavior: 'smooth' });
+          scrolledRef.current = true;
+        }
+      }
+    }
+  });
+};
 
 const App: FunctionComponent = () => {
+  useScrollToLocation();
   return (
     <React.Fragment>
       <div>

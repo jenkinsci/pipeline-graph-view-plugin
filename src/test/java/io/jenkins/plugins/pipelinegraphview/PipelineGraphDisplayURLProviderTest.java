c0a32b4c62dc4e33b76848ffdf35ec8b4e4e94c2
Julien Greffe <jgreffe@cloudbees.com>
2023-07-28 11:20:29 +0200

    [JENKINS-71715] Fix spotless

--- src/test/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphDisplayURLProviderTest.java
+++ src/test/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphDisplayURLProviderTest.java
@@ -1,29 +1,28 @@
 package io.jenkins.plugins.pipelinegraphview;
 
+import hudson.model.Result;
+import io.jenkins.plugins.pipelinegraphview.utils.TestUtils;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
-import io.jenkins.plugins.pipelinegraphview.utils.TestUtils;
-
-import hudson.model.Result;
 
 public class PipelineGraphDisplayURLProviderTest {
 
-  @Rule
-  public JenkinsRule j = new JenkinsRule();
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
 
-  @Issue("JENKINS-71715")
-  @Test
-  public void redirect_urls() throws Exception {
-    WorkflowRun run = TestUtils.createAndRunJob(
-      j, "hello_world_scripted", "helloWorldScriptedPipeline.jenkinsfile", Result.SUCCESS);
+    @Issue("JENKINS-71715")
+    @Test
+    public void redirect_urls() throws Exception {
+        WorkflowRun run = TestUtils.createAndRunJob(
+                j, "hello_world_scripted", "helloWorldScriptedPipeline.jenkinsfile", Result.SUCCESS);
 
-    PipelineGraphDisplayURLProvider provider = new PipelineGraphDisplayURLProvider();
-    String root = provider.getRoot();
-    Assert.assertEquals(provider.getRunURL(run), root + "job/hello_world_scripted/1/pipeline-graph");
-    Assert.assertEquals(provider.getConsoleURL(run), root + "job/hello_world_scripted/1/pipeline-console");
-  }
+        PipelineGraphDisplayURLProvider provider = new PipelineGraphDisplayURLProvider();
+        String root = provider.getRoot();
+        Assert.assertEquals(provider.getRunURL(run), root + "job/hello_world_scripted/1/pipeline-graph");
+        Assert.assertEquals(provider.getConsoleURL(run), root + "job/hello_world_scripted/1/pipeline-console");
+    }
 }

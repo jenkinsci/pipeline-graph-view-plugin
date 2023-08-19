a45151ab0254ec5eceb5abaf6fa5ba586e496483
Devin Nusbaum <dwnusbaum@users.noreply.github.com>
2023-08-19 05:22:59 -0400

    Allow PipelineGraphDisplayURLProvider to activate by default, preserve direct tests/artifacts/changes links, and do not try to handle non-Pipeline builds (#293)

--- src/main/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphDisplayURLProvider.java
+++ src/main/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphDisplayURLProvider.java
@@ -2,12 +2,13 @@ package io.jenkins.plugins.pipelinegraphview;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
-import hudson.Util;
+import hudson.model.Job;
 import hudson.model.Run;
-import org.jenkinsci.plugins.displayurlapi.ClassicDisplayURLProvider;
+import org.jenkinsci.plugins.displayurlapi.DisplayURLProvider;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
-@Extension
-public class PipelineGraphDisplayURLProvider extends ClassicDisplayURLProvider {
+@Extension(ordinal = 50) // Take precedence over Blue Ocean if both are installed and there is no configured provider.
+public class PipelineGraphDisplayURLProvider extends DisplayURLProvider {
 
     @Override
     @NonNull
@@ -24,6 +25,30 @@ public class PipelineGraphDisplayURLProvider extends ClassicDisplayURLProvider {
     @Override
     @NonNull
     public String getRunURL(Run<?, ?> run) {
-        return getRoot() + Util.encode(run.getUrl()) + "pipeline-graph";
+        if (run instanceof WorkflowRun) {
+            return DisplayURLProvider.getDefault().getRunURL(run) + "pipeline-graph";
+        }
+        return DisplayURLProvider.getDefault().getRunURL(run);
+    }
+
+    @Override
+    public String getArtifactsURL(Run<?, ?> run) {
+        return DisplayURLProvider.getDefault().getArtifactsURL(run);
+    }
+
+    @Override
+    public String getChangesURL(Run<?, ?> run) {
+        return DisplayURLProvider.getDefault().getChangesURL(run);
+    }
+
+    @Override
+    public String getTestsURL(Run<?, ?> run) {
+        return DisplayURLProvider.getDefault().getTestsURL(run);
+    }
+
+    @Override
+    public String getJobURL(Job<?, ?> job) {
+        // TODO: Could redirect this to MultiPipelineGraphViewAction
+        return DisplayURLProvider.getDefault().getJobURL(job);
     }
 }

92312674ff5c984df4e99326475ba98ff349c4f6
Tim Jacomb <timjacomb1@gmail.com>
2024-01-14 15:15:40 +0000

    Add configuration option for enabling pipeline stages on job page

--- src/main/java/io/jenkins/plugins/pipelinegraphview/multipipelinegraphview/MultiPipelineGraphViewAction.java
+++ src/main/java/io/jenkins/plugins/pipelinegraphview/multipipelinegraphview/MultiPipelineGraphViewAction.java
@@ -6,6 +6,7 @@ import hudson.model.Action;
 import hudson.security.Permission;
 import hudson.util.HttpResponses;
 import hudson.util.RunList;
+import io.jenkins.plugins.pipelinegraphview.PipelineGraphViewConfiguration;
 import io.jenkins.plugins.pipelinegraphview.utils.PipelineGraph;
 import io.jenkins.plugins.pipelinegraphview.utils.PipelineGraphApi;
 import java.util.ArrayList;
@@ -46,6 +47,9 @@ public class MultiPipelineGraphViewAction implements Action, IconSpec {
         return target.CONFIGURE;
     }
 
+    public boolean isShowGraphOnJobPage() {
+      return PipelineGraphViewConfiguration.get().isShowGraphOnJobPage();
+    }
     @GET
     @WebMethod(name = "tree")
     public HttpResponse getGraph(StaplerRequest req) throws JsonProcessingException {

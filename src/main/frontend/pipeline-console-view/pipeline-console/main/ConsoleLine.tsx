16016e058dcb4e82ee431543f334c6b3755ca28f
stuartrowe <stuartrowe@gmail.com>
2024-03-25 01:24:48 -0700

    Use callback to retrieve ConsoleLine item height

--- src/main/frontend/pipeline-console-view/pipeline-console/main/ConsoleLine.tsx
+++ src/main/frontend/pipeline-console-view/pipeline-console/main/ConsoleLine.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { forwardRef } from "react";
+import { useEffect, useRef } from "react";
 import { makeReactChildren, tokenizeANSIString } from "./Ansi";
 
 export interface ConsoleLineProps {
@@ -7,36 +7,45 @@ export interface ConsoleLineProps {
   content: string;
   stepId: string;
   startByte: number;
+  heightCallback: (height: number) => void;
 }
 
 // Console output line
-export const ConsoleLine = forwardRef<HTMLDivElement, React.PropsWithChildren<ConsoleLineProps>>(function ConsoleLine(props: ConsoleLineProps, ref) {
+export const ConsoleLine = (props: ConsoleLineProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const height = ref.current ? ref.current.getBoundingClientRect().height : 0;
+    props.heightCallback(height);
+  }, []);
+
   return (
-    <>
-      <pre
+    <pre
+      className="console-output-line"
+      key={`console-line-pre${props.lineNumber}`}
+    >
+      <div
+        className="console-output-line-anchor"
+        id={`log-${props.lineNumber}`}
+        key={`${props.lineNumber}-anchor`}
+      />
+      <div
         className="console-output-line"
-        key={`console-line-pre${props.lineNumber}`}
+        key={`${props.lineNumber}-body`}
+        ref={ref}
       >
-        <div
-          className="console-output-line-anchor"
-          id={`log-${props.lineNumber}`}
-          key={`${props.lineNumber}-anchor`}
-        />
-        <div className="console-output-line" key={`${props.lineNumber}-body`} ref={ref}>
-          <a
-            className="console-line-number"
-            href={`?start-byte=${props.startByte}&selected-node=${props.stepId}#log-${props.lineNumber}`} //`}
-          >
-            {props.lineNumber}
-          </a>
-          <div className="console-text">
-            {makeReactChildren(
-              tokenizeANSIString(props.content),
-              `${props.stepId}-${props.lineNumber}`
-            )}
-          </div>
+        <a
+          className="console-line-number"
+          href={`?start-byte=${props.startByte}&selected-node=${props.stepId}#log-${props.lineNumber}`} //`}
+        >
+          {props.lineNumber}
+        </a>
+        <div className="console-text">
+          {makeReactChildren(
+            tokenizeANSIString(props.content),
+            `${props.stepId}-${props.lineNumber}`
+          )}
         </div>
-      </pre>
-    </>
-  )
-});
+      </div>
+    </pre>
+  );
+};

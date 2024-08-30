f62a38ef7bded1add2108330eae6aeee2266c0af
Michael Tughan <michael.tughan@soti.net>
2024-08-29 22:15:22 -0400

    Support parameterized builds in rebuild button
    Allow parameterized builds to be started in the rebuild button by
    redirecting the user to the build with parameters page so they can start
    a new build.

--- src/main/webapp/js/build.js
+++ src/main/webapp/js/build.js
@@ -3,18 +3,23 @@ const rebuildButton = document.getElementById('pgv-rebuild');
 if (rebuildButton) {
   rebuildButton.addEventListener('click', event => {
     event.preventDefault();
-    fetch(`${rebuildButton.dataset.buildPath}?delay=0sec`, {
-      method: 'post',
-      headers: {
-        [document.head.dataset.crumbHeader]: document.head.dataset.crumbValue
-      }
-    })
-      .then(res => {
-        if (!res.ok) {
-          console.error('Build failed', res);
-        } else {
-          window.hoverNotification(rebuildButton.dataset.successMessage, rebuildButton);
+    const buildUrl = `${rebuildButton.dataset.buildPath}?delay=0sec`
+    if (rebuildButton.dataset.parameterized === 'true') {
+      window.location.href = buildUrl
+    } else {
+      fetch(buildUrl, {
+        method: 'post',
+        headers: {
+          [document.head.dataset.crumbHeader]: document.head.dataset.crumbValue
         }
       })
+        .then(res => {
+          if (!res.ok) {
+            console.error('Build failed', res);
+          } else {
+            window.hoverNotification(rebuildButton.dataset.successMessage, rebuildButton);
+          }
+        })
+    }
   })
 }

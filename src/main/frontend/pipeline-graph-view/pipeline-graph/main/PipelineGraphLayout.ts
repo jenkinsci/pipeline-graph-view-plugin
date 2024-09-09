f31a56574fc9654dce95d6f29c643d3747f66a9a
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-09-09 23:47:23 +0100

    Update PipelineGraphLayout.ts

--- src/main/frontend/pipeline-graph-view/pipeline-graph/main/PipelineGraphLayout.ts
+++ src/main/frontend/pipeline-graph-view/pipeline-graph/main/PipelineGraphLayout.ts
@@ -8,7 +8,7 @@ import {
   NodeInfo,
 } from "./PipelineGraphModel";
 
-export const sequentialStagesLabelOffset = 70;
+export const sequentialStagesLabelOffset = 110;
 
 /**
  * Main process for laying out the graph. Creates and positions markers for each component, but creates no components.

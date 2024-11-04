3ae5b58435029fc39833e7a4a109707e0fdb2d39
Hammie <sam@hammie.be>
2024-11-04 15:08:11 +0100

    Take root url into account for agent links

--- src/main/frontend/pipeline-console-view/pipeline-console/main/StageNodeLink.tsx
+++ src/main/frontend/pipeline-console-view/pipeline-console/main/StageNodeLink.tsx
@@ -4,9 +4,16 @@ export interface StageNodeLinkProps {
     agent: string;
 }
 
+function getAgentUrl(name: string) {
+    // Wrap built-in in brackets
+    const id = name == "built-in" ? "(built-in)" : name;
+    const rootPath = document.head.dataset.rootUrl
+    return `${rootPath}/computer/${id}/`;
+}
+
 const StageNodeLink = ({agent}: StageNodeLinkProps) => {
     const agentName = agent == "built-in" ? "Jenkins" : agent;
-    const href = `../../../../computer/${agent == "built-in" ? "(built-in)" : agent }/`;
+    const href = getAgentUrl(agent);
     return <>
         Running on <a href={href}>{agentName}</a>
     </>

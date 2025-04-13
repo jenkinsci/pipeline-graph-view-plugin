8176d68bb4ffd0f7b4bb027786e3e90390052e7a
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-04-13 19:21:11 +0100

    Polish

--- src/main/frontend/pipeline-console-view/app.tsx
+++ src/main/frontend/pipeline-console-view/app.tsx
@@ -1,44 +1,10 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { lazy } from "react";
-import Placeholder from "./pipeline-console/main/Placeholder";
 
 const PipelineConsole = lazy(
   () => import("./pipeline-console/main/PipelineConsole"),
 );
 
 export default function App() {
-  function ConsolePlaceholder() {
-    return (
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "300px 1fr",
-          gap: "1rem",
-        }}
-      >
-        <div
-          style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}
-        >
-          <Placeholder height={2} />
-          <Placeholder height={2} />
-          <Placeholder height={2} />
-          <Placeholder height={2} />
-        </div>
-        <div
-          style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}
-        >
-          <Placeholder height={2.5} />
-          <Placeholder height={10} />
-          <Placeholder height={10} />
-          <Placeholder height={10} />
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <Suspense fallback={<ConsolePlaceholder />}>
-      <PipelineConsole />
-    </Suspense>
-  );
+  return <PipelineConsole />;
 }

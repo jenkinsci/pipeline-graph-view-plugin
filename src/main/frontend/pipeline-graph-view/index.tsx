c843dabbbf1a748fb23a81f5812f28254c31d54e
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-04-15 11:14:01 +0100

    Remove "environment reset due to Prototype" as Prototype has been removed from core

--- src/main/frontend/pipeline-graph-view/index.tsx
+++ src/main/frontend/pipeline-graph-view/index.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 import App from "./app";
 
-import { resetEnvironment } from "../common/reset-environment";
-
-resetEnvironment();
 const rootElement = document.getElementById("graph");
 if (!rootElement) throw new Error("Failed to find the 'graph' element");
 const root = ReactDOMClient.createRoot(rootElement);

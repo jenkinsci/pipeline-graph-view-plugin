60cdbff02762c75d9070fda622062acc16cb2ee7
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-04-19 17:15:07 +0100

    Fix some tests

--- src/main/frontend/pipeline-console-view/pipeline-console/main/PipelineConsole.spec.tsx
+++ src/main/frontend/pipeline-console-view/pipeline-console/main/PipelineConsole.spec.tsx
@@ -1,22 +1,19 @@
 /** * @jest-environment jsdom */
 
+(global as any).TextEncoder = require("util").TextEncoder;
+
 import "@testing-library/jest-dom";
 import React from "react";
 import { default as PipelineConsole } from "./PipelineConsole";
 import DataTreeView from "./DataTreeView";
 import StageView, { StageViewProps } from "./StageView";
-import { StepInfo, StepLogBufferInfo } from "./PipelineConsoleModel";
 import { render } from "@testing-library/react";
 import { RunStatus } from "../../../common/RestClient";
 import {
   defaultStagesList,
   allSuccessfulStepList,
-  multipleErrorsStepList,
-  multipleRunningSteps,
   findStage,
   findStageSteps,
-  runningStepList,
-  unstableThenFailureStepList,
 } from "./TestData";
 
 // This is used to allow 'getConsoleTextOffset' to return different values.
@@ -74,100 +71,6 @@ jest.mock("./StageView", () => {
     return <div>SimpleStageView - selected: {props.stage?.id ?? ""}</div>;
   });
 });
-
-// describe("getDefaultSelectedStep", () => {
-//   it("selects last successful step", async () => {
-//     const selectedStep = getDefaultSelectedStep(
-//       allSuccessfulStepList,
-//     ) as StepInfo;
-//     expect(selectedStep.id).toEqual("21");
-//   });
-//
-//   it("selects first errored step", async () => {
-//     const selectedStep = getDefaultSelectedStep(
-//       multipleErrorsStepList,
-//     ) as StepInfo;
-//     expect(selectedStep.id).toEqual("11");
-//   });
-//
-//   it("selects errored step over unstable", async () => {
-//     const selectedStep = getDefaultSelectedStep(
-//       unstableThenFailureStepList,
-//     ) as StepInfo;
-//     expect(selectedStep.id).toEqual("12");
-//   });
-//
-//   it("selects running step over unstable", async () => {
-//     const selectedStep = getDefaultSelectedStep(runningStepList) as StepInfo;
-//     expect(selectedStep.id).toEqual("12");
-//   });
-//
-//   it("selects first running step", async () => {
-//     const selectedStep = getDefaultSelectedStep(
-//       multipleRunningSteps,
-//     ) as StepInfo;
-//     expect(selectedStep.id).toEqual("10");
-//   });
-//
-//   it("returns null when there are no steps", async () => {
-//     const selectedStep = getDefaultSelectedStep([]) as StepInfo;
-//     expect(selectedStep).toEqual(null);
-//   });
-// });
-
-// describe("updateStepBuffer", () => {
-//   it("resets buffer when startByte is 0", async () => {
-//     // Prime console text buffer with data.
-//     let oldText = "This should be overridden";
-//     let stepBuffer = {
-//       lines: [oldText] as string[],
-//       startByte: 0,
-//       endByte: oldText.length,
-//       stepId: "1",
-//     } as StepLogBufferInfo;
-//     // Request the new log from the beginning.
-//     let previousConsoleText = "This is some dummy text for step '1'";
-//     getConsoleText.mockReturnValueOnce(previousConsoleText);
-//     stepBuffer = updateStepBuffer("1", 0, stepBuffer);
-//     await Promise.resolve();
-//     expect(stepBuffer?.lines).toEqual([previousConsoleText]);
-//   });
-//
-//   it("appends to buffer when startByte is non-zero", async () => {
-//     // Prime console text buffer with data.
-//     let previousConsoleText = "Dummy Text";
-//     let stepBuffer = {
-//       lines: [previousConsoleText] as string[],
-//       startByte: 0,
-//       endByte: previousConsoleText.length,
-//       stepId: "1",
-//     } as StepLogBufferInfo;
-//     // Request the new log from the beginning.
-//     getConsoleText.mockReturnValue(previousConsoleText);
-//     stepBuffer = updateStepBuffer("1", previousConsoleText.length, stepBuffer);
-//     await Promise.resolve();
-//     expect(stepBuffer?.lines).toEqual([
-//       previousConsoleText,
-//       previousConsoleText,
-//     ]);
-//   });
-//
-//   it("ignores trailing whitespace", async () => {
-//     // Prime console text buffer with data.
-//     let previousConsoleText = "Dummy Text";
-//     let stepBuffer = {
-//       lines: [] as string[],
-//       startByte: 0,
-//       endByte: 0,
-//       stepId: "1",
-//     } as StepLogBufferInfo;
-//     // Request the new log from the beginning.
-//     getConsoleText.mockReturnValue(previousConsoleText + "\n\n\n\n\n");
-//     stepBuffer = updateStepBuffer("1", 0, stepBuffer);
-//     await Promise.resolve();
-//     expect(stepBuffer?.lines).toEqual([previousConsoleText]);
-//   });
-// });
 
 describe("PipelineConsole", () => {
   it("Passes expected params stages to DataTreeView", async () => {

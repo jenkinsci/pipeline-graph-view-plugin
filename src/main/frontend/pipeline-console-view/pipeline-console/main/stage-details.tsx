4749b013630bfc1dc215e745121a538375cdbe08
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-04-23 10:29:46 +0100

    Update stage-details.tsx

--- src/main/frontend/pipeline-console-view/pipeline-console/main/stage-details.tsx
+++ src/main/frontend/pipeline-console-view/pipeline-console/main/stage-details.tsx
@@ -50,25 +50,27 @@ export default function StageDetails({ stage }: StageDetailsProps) {
           {total(stage.totalDurationMillis)}
         </li>
         <li>
-          <Tooltip text={exact(stage.startTimeMillis)}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path
-                d="M256 64C150 64 64 150 64 256s86 192 192 192 192-86 192-192S362 64 256 64z"
-                fill="none"
-                stroke="currentColor"
-                strokeMiterlimit="10"
-                strokeWidth="32"
-              />
-              <path
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="32"
-                d="M256 128v144h96"
-              />
-            </svg>
-            {started(stage.startTimeMillis)}
+          <Tooltip content={exact(stage.startTimeMillis)}>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path
+                  d="M256 64C150 64 64 150 64 256s86 192 192 192 192-86 192-192S362 64 256 64z"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit="10"
+                  strokeWidth="32"
+                />
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="32"
+                  d="M256 128v144h96"
+                />
+              </svg>
+              {started(stage.startTimeMillis)}
+            </span>
           </Tooltip>
         </li>
         {stage.pauseDurationMillis !== 0 && (

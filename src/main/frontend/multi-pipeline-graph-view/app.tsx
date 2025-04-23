465a26c5239080612304ae263a52e1600984413c
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-04-23 10:50:52 +0100

    Revert "Add translations to other pages"
    This reverts commit 4dc068435a9d36f3fa25f677a177b2be56a22560.

--- src/main/frontend/multi-pipeline-graph-view/app.tsx
+++ src/main/frontend/multi-pipeline-graph-view/app.tsx
@@ -5,14 +5,11 @@ import { MultiPipelineGraph } from "./multi-pipeline-graph/main";
 
 import "./app.scss";
 import "./multi-pipeline-graph/styles/main.scss";
-import { I18NProvider } from "../common/i18n/i18n-provider";
 
 const App: FunctionComponent = () => {
   return (
     <div>
-      <I18NProvider>
-        <MultiPipelineGraph />
-      </I18NProvider>
+      <MultiPipelineGraph />
     </div>
   );
 };

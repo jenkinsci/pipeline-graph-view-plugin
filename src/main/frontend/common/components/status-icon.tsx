dc28925d24b5102664969cdfe47a431bb81b2e4d
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-04-25 11:08:46 +0100

    Update status-icon.tsx

--- src/main/frontend/common/components/status-icon.tsx
+++ src/main/frontend/common/components/status-icon.tsx
@@ -113,10 +113,26 @@ export default function StatusIcon({
       </Group>
 
       <Group currentStatus={status} status={Result.skipped}>
-        <path
-          d="M320 176a16 16 0 00-16 16v53l-111.68-67.44a10.78 10.78 0 00-16.32 9.31v138.26a10.78 10.78 0 0016.32 9.31L304 267v53a16 16 0 0032 0V192a16 16 0 00-16-16z"
-          fill="var(--color)"
-        />
+        <g transform="scale(0.9)">
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="32"
+            d="M216 352l96-96-96-96"
+            transform="translate(-50, 0)"
+          />
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="32"
+            d="M216 352l96-96-96-96"
+            transform="translate(70, 0)"
+          />
+        </g>
       </Group>
 
       <Group currentStatus={status} status={Result.paused}>

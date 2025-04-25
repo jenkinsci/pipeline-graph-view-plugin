7ffa3e6541e55db3e1879836f8abfc7aa3d5bf42
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-04-25 13:47:17 +0100

    init

---
+++ src/test/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphViewFullTest.java
@@ -0,0 +1,14 @@
+package io.jenkins.plugins.pipelinegraphview;
+
+import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+
+@WithJenkins
+class PipelineGraphViewFullTest {
+
+    @Test
+    void doTheThing(JenkinsRule j) {
+
+    }
+}

6c099a3b5cae08b46cc62b5bec85b14f867ecf74
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2025-04-25 10:41:01 +0100

    Remove legacy visitor implementations (#653)

--- src/main/java/io/jenkins/plugins/pipelinegraphview/utils/PipelineGraphApi.java
+++ src/main/java/io/jenkins/plugins/pipelinegraphview/utils/PipelineGraphApi.java
@@ -7,7 +7,6 @@ import hudson.model.CauseAction;
 import hudson.model.Item;
 import hudson.model.Queue;
 import io.jenkins.plugins.pipelinegraphview.treescanner.PipelineNodeGraphAdapter;
-import io.jenkins.plugins.pipelinegraphview.utils.legacy.PipelineNodeGraphVisitor;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -124,107 +123,6 @@ public class PipelineGraphApi {
         return new PipelineGraph(stageResults, execution.isComplete());
     }
 
-    /*
-     * Create a shallow Tree from the GraphVisitor.
-     * This creates a shallower representation of the DAG - similar to . Here
-     * children stages will become siblings of the parent.
-     * Parent -> ChildA -> ChildB
-     * instead of:
-     * Parent
-     * |-> ChildA -> ChildC
-     * Parallel branches will still appear as children ther parent stage.
-     * Original source:
-     * https://github.com/jenkinsci/workflow-support-plugin/blob/master/src/main/
-     * java/org/jenkinsci/plugins/workflow/support/visualization/table/
-     * FlowGraphTable.java#L126
-     */
-    private PipelineGraph createShallowTree(PipelineGraphBuilderApi builder) {
-        List<PipelineStageInternal> stages = getPipelineNodes(builder);
-        List<String> topLevelStageIds = new ArrayList<>();
-
-        // id => stage
-        Map<String, PipelineStageInternal> stageMap = stages.stream()
-                .collect(Collectors.toMap(
-                        PipelineStageInternal::getId, stage -> stage, (u, v) -> u, LinkedHashMap::new));
-
-        Map<String, List<String>> stageToChildrenMap = new HashMap<>();
-
-        FlowExecution execution = run.getExecution();
-        if (execution == null) {
-            // If we don't have an execution - e.g. if the Pipeline has a syntax error -
-            // then return an
-            // empty graph.
-            return new PipelineGraph(new ArrayList<>(), false);
-        }
-        stages.forEach(stage -> {
-            try {
-                FlowNode stageNode = execution.getNode(stage.getId());
-                if (stageNode == null) {
-                    return;
-                }
-                List<String> ancestors = getAncestors(stage, stageMap);
-                String treeParentId = null;
-                // Compare the list of GraphVistor ancestors to the IDs of the enclosing node in
-                // the
-                // execution.
-                // If a node encloses another node, it means it's a tree parent, so the first
-                // ancestor
-                // ID we find
-                // which matches an enclosing node then it's the stages tree parent.
-                List<String> enclosingIds = stageNode.getAllEnclosingIds();
-                for (String ancestorId : ancestors) {
-                    if (enclosingIds.contains(ancestorId)) {
-                        treeParentId = ancestorId;
-                        break;
-                    }
-                }
-                if (treeParentId != null) {
-                    List<String> childrenOfParent = stageToChildrenMap.getOrDefault(treeParentId, new ArrayList<>());
-                    childrenOfParent.add(stage.getId());
-                    stageToChildrenMap.put(treeParentId, childrenOfParent);
-                } else {
-                    // If we can't find a matching parent in the execution and GraphVistor then this
-                    // is a
-                    // top level node.
-                    stageToChildrenMap.put(stage.getId(), new ArrayList<>());
-                    topLevelStageIds.add(stage.getId());
-                }
-            } catch (IOException ex) {
-                logger.error(
-                        "Caught a {} when trying to find parent of stage '{}'",
-                        ex.getClass().getSimpleName(),
-                        stage.getName());
-            }
-        });
-
-        String runUrl = run.getUrl();
-        List<PipelineStage> stageResults = stageMap.values().stream()
-                .map(pipelineStageInternal -> {
-                    List<PipelineStage> children =
-                            stageToChildrenMap.getOrDefault(pipelineStageInternal.getId(), emptyList()).stream()
-                                    .map(mapper(stageMap, stageToChildrenMap))
-                                    .collect(Collectors.toList());
-
-                    return pipelineStageInternal.toPipelineStage(children, runUrl);
-                })
-                .filter(stage -> topLevelStageIds.contains(stage.getId()))
-                .collect(Collectors.toList());
-        return new PipelineGraph(stageResults, execution.isComplete());
-    }
-
-    private List<String> getAncestors(PipelineStageInternal stage, Map<String, PipelineStageInternal> stageMap) {
-        List<String> ancestors = new ArrayList<>();
-        if (!stage.getParents().isEmpty()) {
-            String parentId = stage.getParents().get(0); // Assume one parent.
-            ancestors.add(parentId);
-            if (stageMap.containsKey(parentId)) {
-                PipelineStageInternal parent = stageMap.get(parentId);
-                ancestors.addAll(getAncestors(parent, stageMap));
-            }
-        }
-        return ancestors;
-    }
-
     private static String getStageNode(FlowNodeWrapper flowNodeWrapper) {
         FlowNode flowNode = flowNodeWrapper.getNode();
         DepthFirstScanner scan = new DepthFirstScanner();
@@ -270,27 +168,5 @@ public class PipelineGraphApi {
 
     public PipelineGraph createTree() {
         return createTree(new PipelineNodeGraphAdapter(run));
-    }
-
-    /*
-     * Get a shallower (less nested) representation of the DAG.
-     * This might miss some information, but looks more like the previous
-     * implementation.
-     * Currently used for the legacy tests that check the adapter output looks like
-     * the legacy one.
-     */
-    protected PipelineGraph createShallowTree() {
-        // Make add non-parallel branches siblings to their parents (remove nesting of
-        // regular stages).
-        return createShallowTree(new PipelineNodeGraphAdapter(run));
-    }
-
-    /**
-     * Creates the tree using the legacy PipelineNodeGraphVisitor class.
-     * This is useful for testing and could be useful for bridging the gap between
-     * representations.
-     */
-    protected PipelineGraph createLegacyTree() {
-        return createShallowTree(new PipelineNodeGraphVisitor(run));
     }
 }

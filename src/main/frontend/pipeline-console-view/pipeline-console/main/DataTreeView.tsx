31637cbbc0caa33500e477da5acd384821baf2ae
Tim Jacomb <21194782+timja@users.noreply.github.com>
2025-04-25 18:13:00 +0100

    Use correct react property names (#660)

--- src/main/frontend/pipeline-console-view/pipeline-console/main/DataTreeView.tsx
+++ src/main/frontend/pipeline-console-view/pipeline-console/main/DataTreeView.tsx
@@ -35,15 +35,15 @@ export default function DataTreeView({
                 d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
                 fill="none"
                 stroke="currentColor"
-                stroke-miterlimit="10"
-                stroke-width="32"
+                strokeMiterlimit="10"
+                strokeWidth="32"
               />
               <path
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-miterlimit="10"
-                stroke-width="32"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                strokeWidth="32"
                 d="M338.29 338.29L448 448"
               />
             </svg>

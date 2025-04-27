e4338f7ccc4ba53b61d85e3fec36454533b23f5e
Felipe Santos <felipecassiors@gmail.com>
2025-04-27 01:36:30 -0300

    Fix import base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // TODO: remove hardcoded /jenkins/
+  base: "/jenkins/plugin/pipeline-graph-view/js/bundles/",
   build: {
     rollupOptions: {
       input: {

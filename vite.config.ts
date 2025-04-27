50eb7fdc298f258aea77d1be2dcac8e810625255
Felipe Santos <felipecassiors@gmail.com>
2025-04-27 03:42:58 -0300

    Fix missing jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import checker from 'vite-plugin-checker';
+import checker from "vite-plugin-checker";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({

becbf2e13a1f6be3f84f21733dc80c82838f8ef8
Felipe Santos <felipecassiors@gmail.com>
2025-04-27 17:08:29 +0000

    Run tsc instead of vite-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import checker from "vite-plugin-checker";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   base: "./",
   plugins: [
     react(),
-    checker({
-      typescript: true,
-    }),
     cssInjectedByJsPlugin({
       relativeCSSInjection: true,
     }),

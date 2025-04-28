210c5e8002102781b7588dc1d8700726cf3521f5
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-04-28 17:36:15 +0100

    Add placeholder expand button

--- src/main/frontend/pipeline-console-view/pipeline-console/main/components/stages.tsx
+++ src/main/frontend/pipeline-console-view/pipeline-console/main/components/stages.tsx
@@ -6,6 +6,7 @@ import {
 } from "../../../../pipeline-graph-view/pipeline-graph/main/index.ts";
 import React, { CSSProperties, useEffect, useState } from "react";
 import "./stages.scss";
+import Tooltip from "../../../../common/components/tooltip.tsx";
 
 export default function Stages({
   stages,
@@ -39,6 +40,29 @@ export default function Stages({
     >
       <div className={"test-floaty-material pgv-graph-view-thing__heading"}>
         Stages
+      </div>
+      <div className={"test-floaty-material pgw-fullscreen-controls"}>
+        <Tooltip content={"Expand"}>
+          <button
+            className={"jenkins-button jenkins-button--tertiary"}
+            onClick={() => alert("This will expand")}
+          >
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 4H15C15.5523 4 16 4.44772 16 5V10M10 16H5C4.44772 16 4 15.5523 4 15V10"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              />
+            </svg>
+          </button>
+        </Tooltip>
       </div>
       <PipelineGraph stages={stages} onStageSelect={handleStageSelect} />
     </div>

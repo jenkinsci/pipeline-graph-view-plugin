42d998fde36dcb4b3db43ae865c6f91356042957
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-04-28 11:35:32 +0100

    Update skipped icon (#657)

--- src/main/frontend/common/components/status-icon.tsx
+++ src/main/frontend/common/components/status-icon.tsx
@@ -113,10 +113,26 @@ export default function StatusIcon({
       </Group>
 
       <Group currentStatus={status} status={Result.skipped}>
-        <path
-          d="M320 176a16 16 0 00-16 16v53l-111.68-67.44a10.78 10.78 0 00-16.32 9.31v138.26a10.78 10.78 0 0016.32 9.31L304 267v53a16 16 0 0032 0V192a16 16 0 00-16-16z"
-          fill="var(--color)"
-        />
+        <g transform="scale(0.8)">
+          <path
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="36"
+            d="M216 352l96-96-96-96"
+            transform="translate(-55, 0)"
+          />
+          <path
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="36"
+            d="M216 352l96-96-96-96"
+            transform="translate(75, 0)"
+          />
+        </g>
       </Group>
 
       <Group currentStatus={status} status={Result.paused}>

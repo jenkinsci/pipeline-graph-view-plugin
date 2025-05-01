cf18c85f6508a553cdcd11c6c672611484ecaa69
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-05-01 14:42:00 +0100

    linting
    Signed-off-by: lewisbirks <22620804+lewisbirks@users.noreply.github.com>

--- src/main/frontend/common/i18n/index.ts
+++ src/main/frontend/common/i18n/index.ts
@@ -1,4 +1,4 @@
-export { Messages, ResourceBundleName } from "./messages.ts";
-export type { ResourceBundle } from "./messages.ts";
 export * from "./i18n-provider.tsx";
 export type { MessageContext } from "./message-format.ts";
+export type { ResourceBundle } from "./messages.ts";
+export { Messages, ResourceBundleName } from "./messages.ts";

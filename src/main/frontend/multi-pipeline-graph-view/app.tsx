8e77d77ef2b4c035a592a10bdb8aab422f4e1e8f
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-05-01 16:22:20 +0100

    Propagate locale through application
    Signed-off-by: lewisbirks <22620804+lewisbirks@users.noreply.github.com>

--- src/main/frontend/multi-pipeline-graph-view/app.tsx
+++ src/main/frontend/multi-pipeline-graph-view/app.tsx
@@ -3,19 +3,23 @@ import "./multi-pipeline-graph/styles/main.scss";
 
 import { FunctionComponent } from "react";
 
-import { I18NProvider } from "../common/i18n/i18n-provider.tsx";
-import { ResourceBundleName } from "../common/i18n/messages.ts";
+import {
+  I18NProvider,
+  LocaleProvider,
+  ResourceBundleName,
+} from "../common/i18n/index.ts";
 import { MultiPipelineGraph } from "./multi-pipeline-graph/main/MultiPipelineGraph.tsx";
 
 const App: FunctionComponent = () => {
-  const locale =
-    document.getElementById("multiple-pipeline-root")?.dataset.userLocale ??
-    "en";
+  const locale = document.getElementById("multiple-pipeline-root")!.dataset
+    .userLocale!;
   return (
     <div>
-      <I18NProvider bundles={[ResourceBundleName.messages]} locale={locale}>
-        <MultiPipelineGraph />
-      </I18NProvider>
+      <LocaleProvider locale={locale}>
+        <I18NProvider bundles={[ResourceBundleName.messages]}>
+          <MultiPipelineGraph />
+        </I18NProvider>
+      </LocaleProvider>
     </div>
   );
 };

2f82cbd2f1f10440b2bb8ccac122594b04663822
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-05-01 14:16:28 +0100

    Remove @messageformat/core
    Signed-off-by: lewisbirks <22620804+lewisbirks@users.noreply.github.com>

---
+++ src/main/frontend/common/i18n/index.ts
@@ -0,0 +1,4 @@
+export { Messages, ResourceBundleName } from "./messages.ts";
+export type { ResourceBundle } from "./messages.ts";
+export * from "./i18n-provider.tsx";
+export type { MessageContext } from "./message-format.ts";

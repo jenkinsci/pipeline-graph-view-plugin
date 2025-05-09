534cf2067b19d940ef705ab8a9c561af5bcc0264
Stuart Rowe <stuartrowe@gmail.com>
2025-05-09 16:15:43 -0700

    Fix get_changes test

--- src/test/java/io/jenkins/plugins/pipelinegraphview/cards/items/ChangesRunDetailsItemTest.java
+++ src/test/java/io/jenkins/plugins/pipelinegraphview/cards/items/ChangesRunDetailsItemTest.java
@@ -42,7 +42,7 @@ class ChangesRunDetailsItemTest {
         RunDetailsItem.RunDetail changesDetails = (RunDetailsItem.RunDetail) detailsItem.get();
 
         assertEquals(
-                new RunDetailsItem.ItemContent.LinkContent(j.getURL() + run.getUrl(), Messages.changes()),
+                new RunDetailsItem.ItemContent.LinkContent("../changes", Messages.changes()),
                 changesDetails.content());
         assertEquals("symbol-code-slash-outline plugin-ionicons-api", changesDetails.icon());
     }

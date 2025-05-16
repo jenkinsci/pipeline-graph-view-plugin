e244c2ace457f5e8215b5bb59e2381edb53505a1
Tim Jacomb <timjacomb1@gmail.com>
2025-05-16 08:53:48 +0100

    Format

--- src/main/frontend/pipeline-console-view/pipeline-console/main/PipelineConsole.tsx
+++ src/main/frontend/pipeline-console-view/pipeline-console/main/PipelineConsole.tsx
@@ -9,12 +9,12 @@ import Stages from "./components/stages.tsx";
 import StagesCustomization from "./components/stages-customization.tsx";
 import DataTreeView from "./DataTreeView.tsx";
 import { useStepsPoller } from "./hooks/use-steps-poller.ts";
+import { NoStageStepsFallback } from "./NoStageStepsFallback.tsx";
 import { useLayoutPreferences } from "./providers/user-preference-provider.tsx";
 import ScrollToTopBottom from "./scroll-to-top-bottom.tsx";
 import SplitView from "./split-view.tsx";
 import StageView from "./StageView.tsx";
 import { CONSOLE, DOCUMENT } from "./symbols.tsx";
-import { NoStageStepsFallback } from "./NoStageStepsFallback.tsx";
 
 export default function PipelineConsole() {
   const rootElement = document.getElementById("console-pipeline-root");

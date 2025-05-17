4d9424f3716c706769413cab51f268c13551ff1c
Stuart Rowe <stuartrowe@gmail.com>
2025-05-17 15:12:55 -0700

    Rework test

--- src/test/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphViewCancelTest.java
+++ src/test/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphViewCancelTest.java
@@ -1,14 +1,16 @@
 package io.jenkins.plugins.pipelinegraphview;
 
-import static org.awaitility.Awaitility.await;
-import static org.hamcrest.CoreMatchers.is;
-
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Response;
 import hudson.model.Result;
+import hudson.model.queue.QueueTaskFuture;
 import io.jenkins.plugins.pipelinegraphview.consoleview.PipelineConsoleViewAction;
 import io.jenkins.plugins.pipelinegraphview.utils.TestUtils;
+
+import static org.junit.Assert.assertTrue;
+
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.test.steps.SemaphoreStep;
 import org.junit.jupiter.api.Test;
@@ -20,22 +22,21 @@ class PipelineGraphViewCancelTest {
 
     @Test
     void cancelButtonCancelsBuild(JenkinsRule j) throws Exception {
-        WorkflowRun run = TestUtils.createAndRunJob(j, "indefiniteWait", "indefiniteWait.jenkinsfile", Result.SUCCESS);
+        QueueTaskFuture<WorkflowRun> futureRun = TestUtils.createAndRunJobNoWait(j, "indefiniteWait", "indefiniteWait.jenkinsfile");
+        WorkflowRun run = futureRun.waitForStart();
+        SemaphoreStep.waitForStart("wait/1", run);
+        cancelRun(j, run);
+        SemaphoreStep.success("wait/1", null);
+        j.assertBuildStatus(Result.ABORTED, j.waitForCompletion(run));
+    }
 
+    private static void cancelRun(JenkinsRule j, WorkflowRun run) throws Exception {
         try (Playwright playwright = Playwright.create();
                 Browser browser = playwright.chromium().launch()) {
             Page page = browser.newPage();
             String urlName = new PipelineConsoleViewAction(run).getUrlName();
             page.navigate(j.getURL() + run.getUrl() + urlName);
-
             page.click("#pgv-cancel");
-
-            SemaphoreStep.waitForStart("wait/1", run);
-            await().until(() -> run.isBuilding(), is(false));
-
-            SemaphoreStep.success("wait/1", null);
-
-            j.assertBuildStatus(Result.ABORTED, run);
         }
     }
 }

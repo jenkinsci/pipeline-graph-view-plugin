4752493ef4490a1b38d94c84a5d0a80f8f3fcc88
Stuart Rowe <stuartrowe@gmail.com>
2025-05-17 14:34:06 -0700

    Add cancel button test

---
+++ src/test/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphViewCancelTest.java
@@ -0,0 +1,48 @@
+package io.jenkins.plugins.pipelinegraphview;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Response;
+import hudson.model.Result;
+import io.jenkins.plugins.pipelinegraphview.consoleview.PipelineConsoleViewAction;
+import io.jenkins.plugins.pipelinegraphview.utils.TestUtils;
+import jenkins.test.RunMatchers;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.test.steps.SemaphoreStep;
+import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+
+@WithJenkins
+class PipelineGraphViewCancelTest {
+
+    @Test
+    void cancelButtonCancelsBuild(JenkinsRule j) throws Exception {
+        WorkflowRun run =
+                TestUtils.createAndRunJob(j, "indefiniteWait", "indefiniteWait.jenkinsfile", Result.SUCCESS);
+
+        try (Playwright playwright = Playwright.create();
+                Browser browser = playwright.chromium().launch()) {
+            Page page = browser.newPage();
+            String urlName = new PipelineConsoleViewAction(run).getUrlName();
+            page.navigate(j.getURL() + run.getUrl() + urlName);
+
+            page.click("#pgv-cancel");
+
+            SemaphoreStep.waitForStart("wait/1", run);
+            await().until(() -> run.isBuilding(), is(false));
+
+            SemaphoreStep.success("wait/1", null);
+
+            j.assertBuildStatus(Result.ABORTED, run);
+        }
+    }
+
+}

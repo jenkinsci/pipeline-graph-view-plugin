d3449cdf74ad5fe39612bc828ba064dd9a1dc2d4
Stuart Rowe <stuartrowe@gmail.com>
2025-05-17 15:18:56 -0700

    spotless

--- src/test/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphViewCancelTest.java
+++ src/test/java/io/jenkins/plugins/pipelinegraphview/PipelineGraphViewCancelTest.java
@@ -3,14 +3,10 @@ package io.jenkins.plugins.pipelinegraphview;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.Response;
 import hudson.model.Result;
 import hudson.model.queue.QueueTaskFuture;
 import io.jenkins.plugins.pipelinegraphview.consoleview.PipelineConsoleViewAction;
 import io.jenkins.plugins.pipelinegraphview.utils.TestUtils;
-
-import static org.junit.Assert.assertTrue;
-
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.test.steps.SemaphoreStep;
 import org.junit.jupiter.api.Test;
@@ -22,7 +18,8 @@ class PipelineGraphViewCancelTest {
 
     @Test
     void cancelButtonCancelsBuild(JenkinsRule j) throws Exception {
-        QueueTaskFuture<WorkflowRun> futureRun = TestUtils.createAndRunJobNoWait(j, "indefiniteWait", "indefiniteWait.jenkinsfile");
+        QueueTaskFuture<WorkflowRun> futureRun =
+                TestUtils.createAndRunJobNoWait(j, "indefiniteWait", "indefiniteWait.jenkinsfile");
         WorkflowRun run = futureRun.waitForStart();
         SemaphoreStep.waitForStart("wait/1", run);
         cancelRun(j, run);

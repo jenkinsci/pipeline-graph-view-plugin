e5179ea4fdeb1d6b03b79823779c42ac61effdf3
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2025-06-17 12:02:18 +0100

    Apply prettier formatting to non bundled frontend files (#855)

--- src/main/webapp/js/build.js
+++ src/main/webapp/js/build.js
@@ -1,22 +1,25 @@
-const rerunButton = document.getElementById('pgv-rerun');
+const rerunButton = document.getElementById("pgv-rerun");
 
 if (rerunButton) {
-  rerunButton.addEventListener('click', event => {
+  rerunButton.addEventListener("click", (event) => {
     event.preventDefault();
     const rerunAction = window[`${rerunButton.dataset.proxyName}`];
     rerunAction.doRerun(function (success) {
       const result = success.responseJSON;
       if (result) {
-        notificationBar.show(rerunButton.dataset.successMessage, notificationBar.SUCCESS);
+        notificationBar.show(
+          rerunButton.dataset.successMessage,
+          notificationBar.SUCCESS,
+        );
       }
     });
-  })
+  });
 }
 
-const cancelButton = document.getElementById('pgv-cancel');
+const cancelButton = document.getElementById("pgv-cancel");
 
 if (cancelButton) {
-  cancelButton.addEventListener('click', event => {
+  cancelButton.addEventListener("click", (event) => {
     event.preventDefault();
     const question = cancelButton.getAttribute("data-confirm");
     const execute = function () {
@@ -24,43 +27,46 @@ if (cancelButton) {
       cancelAction.doCancel(function (response) {
         const result = response.responseJSON;
         if (result.status === "ok") {
-          notificationBar.show(cancelButton.dataset.successMessage)
+          notificationBar.show(cancelButton.dataset.successMessage);
         } else {
-          notificationBar.show(result.message, notificationBar.WARNING)
+          notificationBar.show(result.message, notificationBar.WARNING);
         }
       });
-      
     };
 
     if (question != null) {
-      dialog.confirm(question).then(() => {
-        execute();
-        return null;
-      }).catch((error) => {
-        console.error("Error in cancel confirm dialog:", error);
-      })
+      dialog
+        .confirm(question)
+        .then(() => {
+          execute();
+          return null;
+        })
+        .catch((error) => {
+          console.error("Error in cancel confirm dialog:", error);
+        });
     } else {
       execute();
     }
-  })
+  });
 
   function updateCancelButton() {
     const buildCaption = document.querySelector(".jenkins-build-caption");
     const url = buildCaption.dataset.statusUrl;
-    fetch(url).then((rsp) => {
-      if (rsp.ok) {
-        const isBuilding = rsp.headers.get("X-Building");
-        if (isBuilding === "true") {
-          setTimeout(updateCancelButton, 5000);
-        } else {
-          cancelButton.style.display = "none";
+    fetch(url)
+      .then((rsp) => {
+        if (rsp.ok) {
+          const isBuilding = rsp.headers.get("X-Building");
+          if (isBuilding === "true") {
+            setTimeout(updateCancelButton, 5000);
+          } else {
+            cancelButton.style.display = "none";
+          }
         }
-      }
-      return null;
-    })
-    .catch((error) => {
-      console.error("Error fetching build caption statsus:", error);
-    })
+        return null;
+      })
+      .catch((error) => {
+        console.error("Error fetching build caption status:", error);
+      });
   }
   setTimeout(updateCancelButton, 5000);
 }

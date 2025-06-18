cbd46dfd809b5e833083c18f37488d03e376e8d5
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2025-06-18 15:35:36 +0100

    replace promise based polling with await based
    Signed-off-by: Lewis Birks <22620804+lewisbirks@users.noreply.github.com>

--- src/main/webapp/js/build.js
+++ src/main/webapp/js/build.js
@@ -5,43 +5,39 @@ const rerunButton = document.getElementById("pgv-rerun");
 if (rerunButton) {
   rerunButton.addEventListener("click", (event) => {
     event.preventDefault();
-    function redirectToNextBuild(queueId) {
-      function poll() {
-        const interval = 1000;
-        fetch(`nextBuild?queueId=${queueId}`)
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            } else {
-              throw new Error("Failed to fetch next build data");
-            }
-          })
-          .then(({ status, data, message }) => {
-            if (status === "ok") {
-              if (data.nextBuildUrl) {
-                window.location = data.nextBuildUrl;
-                return;
-              }
-            } else {
-              console.warn("Error in next build response:", message);
-            }
-            setTimeout(poll, interval);
-          })
-          .catch((error) => {
-            console.error("Error fetching next build data:", error);
-            setTimeout(poll, interval);
-          });
-      }
 
-      poll();
+    async function redirectToNextBuild(queueId) {
+      while (true) {
+        try {
+          const response = await fetch(`nextBuild?queueId=${queueId}`);
+          console.log(response);
+          if (!response.ok) {
+            throw new Error(
+              `Failed to fetch next build data: ${response.status} - ${response.statusText}`,
+            );
+          }
+          const { status, data, message } = await response.json();
+          if (status === "ok") {
+            if (data?.nextBuildUrl) {
+              window.location = data.nextBuildUrl;
+              break;
+            }
+          } else {
+            console.warn("Error in next build response:", message);
+          }
+        } catch (error) {
+          console.error("Error fetching next build data:", error);
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
     }
 
     const rerunAction = window[`${rerunButton.dataset.proxyName}`];
-    rerunAction.doRerun(function (response) {
+    rerunAction.doRerun(async function (response) {
       const { status, data, message } = response.responseJSON;
       if (status === "ok") {
         notificationBar.show(data.message, notificationBar.SUCCESS);
-        redirectToNextBuild(data.queueId);
+        await redirectToNextBuild(data.queueId);
       } else {
         const failMessage =
           status === "error" && message

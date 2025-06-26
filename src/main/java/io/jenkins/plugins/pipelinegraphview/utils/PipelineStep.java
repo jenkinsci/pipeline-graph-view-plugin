66af9d31e8f1a1d4272b61e02b4b0edac8f95af5
Tim Jacomb <timjacomb1@gmail.com>
2025-06-26 22:46:14 +0100

    Use custom rendering for input step

--- src/main/java/io/jenkins/plugins/pipelinegraphview/utils/PipelineStep.java
+++ src/main/java/io/jenkins/plugins/pipelinegraphview/utils/PipelineStep.java
@@ -4,6 +4,7 @@ import io.jenkins.plugins.pipelinegraphview.analysis.TimingInfo;
 
 public class PipelineStep extends AbstractPipelineNode {
     private final String stageId;
+    private final PipelineInputStep inputStep;
 
     public PipelineStep(
             String id,
@@ -12,12 +13,18 @@ public class PipelineStep extends AbstractPipelineNode {
             String type,
             String title,
             String stageId,
+            PipelineInputStep inputStep,
             TimingInfo timingInfo) {
         super(id, name, state, type, title, timingInfo);
         this.stageId = stageId;
+        this.inputStep = inputStep;
     }
 
     public String getStageId() {
         return stageId;
+    }
+
+    public PipelineInputStep getInputStep() {
+        return inputStep;
     }
 }

88d8bfdbefb89ebae687013a015b6776aaf3a4e7
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2025-07-10 10:17:38 +0100

    use externalizable id for key (it does the same thing anyway)
    Signed-off-by: Lewis Birks <22620804+lewisbirks@users.noreply.github.com>

--- src/main/java/io/jenkins/plugins/pipelinegraphview/utils/CachedPipelineNodeGraphAdaptor.java
+++ src/main/java/io/jenkins/plugins/pipelinegraphview/utils/CachedPipelineNodeGraphAdaptor.java
@@ -7,33 +7,32 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.ConcurrentHashMap;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CachedPipelineNodeGraphAdaptor {
 
     public static final CachedPipelineNodeGraphAdaptor instance = new CachedPipelineNodeGraphAdaptor();
+    private static final Logger log = LoggerFactory.getLogger(CachedPipelineNodeGraphAdaptor.class);
 
-    private final Map<TaskKey, CompletableFuture<PipelineNodeGraphAdapter>> tasks = new ConcurrentHashMap<>();
+    private final Map<String, CompletableFuture<PipelineNodeGraphAdapter>> tasks = new ConcurrentHashMap<>();
 
     private CachedPipelineNodeGraphAdaptor() {}
 
     public PipelineNodeGraphAdapter getFor(WorkflowRun run) {
-        TaskKey key = new TaskKey(run);
+        String key = run.getExternalizableId();
 
-        CompletableFuture<PipelineNodeGraphAdapter> task = tasks.computeIfAbsent(
-                key, (ignored) -> CompletableFuture.supplyAsync(() -> new PipelineNodeGraphAdapter(run)));
+        CompletableFuture<PipelineNodeGraphAdapter> task = tasks.computeIfAbsent(key, (ignored) -> {
+            log.debug("Creating new PipelineNodeGraphAdapter for run: {}", key);
+            return CompletableFuture.supplyAsync(() -> new PipelineNodeGraphAdapter(run));
+        });
 
         try {
             return task.join();
         } catch (CancellationException | CompletionException e) {
-            throw new RuntimeException("Failure computing graph for " + key.job + " - " + key.run, e);
+            throw new RuntimeException("Failure computing graph for " + key, e);
         } finally {
             tasks.remove(key);
-        }
-    }
-
-    private record TaskKey(String job, String run) {
-        TaskKey(WorkflowRun run) {
-            this(run.getParent().getFullName(), run.getId());
         }
     }
 }

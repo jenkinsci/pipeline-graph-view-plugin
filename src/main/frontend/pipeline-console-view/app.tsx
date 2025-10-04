6e7275eb297a7a31f146ad328f54b86528f0a315
Jakob Ackermann <das7pad@outlook.com>
2025-10-04 15:38:54 +0200

    Avoid unnecessary lazy loading of PipelineConsole
    Nothing else starts rendering while PipelineConsole is downloading, so
    there is no point in delaying the download.
    
    Before: 5 round trips, 17s with "Slow 4G" network throttling, 40 req
    1 pipeline-console-view-bundle.js
    2 `- bundle with React.lazy
    3    `- PipelineConsole
    4       `- i18n Messages
    4       `- tree
    4       `- allSteps
    5         `- consoleOutput
    5         `- ConsoleLogStream
    
    After: 4 round trips, 15.8s with "Slow 4G" network throttling, 36 req
    1 pipeline-console-view-bundle.js
    2 `- imports
    3    `- i18n Messages
    3    `- tree
    3    `- allSteps
    4       `- consoleOutput
    4       `- ConsoleLogStream

--- src/main/frontend/pipeline-console-view/app.tsx
+++ src/main/frontend/pipeline-console-view/app.tsx
@@ -1,5 +1,3 @@
-import { lazy } from "react";
-
 import {
   I18NProvider,
   LocaleProvider,
@@ -7,12 +5,9 @@ import {
 } from "../common/i18n/index.ts";
 import { UserPermissionsProvider } from "../common/user/user-permission-provider.tsx";
 import { UserPreferencesProvider } from "../common/user/user-preferences-provider.tsx";
+import PipelineConsole from "./pipeline-console/main/PipelineConsole.tsx";
 import { FilterProvider } from "./pipeline-console/main/providers/filter-provider.tsx";
 import { LayoutPreferencesProvider } from "./pipeline-console/main/providers/user-preference-provider.tsx";
-
-const PipelineConsole = lazy(
-  () => import("./pipeline-console/main/PipelineConsole.tsx"),
-);
 
 export default function App() {
   const locale = document.getElementById("console-pipeline-root")!.dataset

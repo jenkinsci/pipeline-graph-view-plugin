bd0e79e355ba4c17e2733ec2e25f85dd1371d6b2
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2026-04-26 11:45:17 +0100

    Fix test

--- src/main/frontend/setupTests.ts
+++ src/main/frontend/setupTests.ts
@@ -2,9 +2,11 @@ import "@testing-library/jest-dom/vitest";
 
 import { vi } from "vitest";
 
-const IntersectionObserverMock = vi.fn(() => ({
-  disconnect: vi.fn(),
-  observe: vi.fn(),
-}));
+const IntersectionObserverMock = vi.fn(function IntersectionObserverMock() {
+  return {
+    disconnect: vi.fn(),
+    observe: vi.fn(),
+  };
+});
 
 vi.stubGlobal("IntersectionObserver", IntersectionObserverMock);

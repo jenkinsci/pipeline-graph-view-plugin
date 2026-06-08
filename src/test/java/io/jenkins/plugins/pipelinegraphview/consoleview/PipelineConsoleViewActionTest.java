56b1d4bceaf1ae494cb2dc22b50c52a8c7c44b16
Jakob Ackermann <das7pad@outlook.com>
2026-06-08 10:33:12 +0200

    Include exception text in getConsoleText response (#1319)

--- src/test/java/io/jenkins/plugins/pipelinegraphview/consoleview/PipelineConsoleViewActionTest.java
+++ src/test/java/io/jenkins/plugins/pipelinegraphview/consoleview/PipelineConsoleViewActionTest.java
@@ -7,9 +7,13 @@ import hudson.model.Result;
 import io.jenkins.plugins.pipelinegraphview.treescanner.PipelineNodeGraphAdapter;
 import io.jenkins.plugins.pipelinegraphview.utils.FlowNodeWrapper;
 import io.jenkins.plugins.pipelinegraphview.utils.TestUtils;
+import java.io.IOException;
 import java.util.List;
+import org.htmlunit.WebRequest;
+import org.htmlunit.WebResponse;
 import org.htmlunit.html.DomElement;
 import org.htmlunit.html.HtmlPage;
+import org.htmlunit.util.UrlUtils;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.Issue;
@@ -32,6 +36,32 @@ class PipelineConsoleViewActionTest {
         PipelineConsoleViewAction consoleAction = new PipelineConsoleViewAction(run);
         String message = consoleAction.getNodeExceptionText(errorStep.getId());
         assertThat(message, equalTo("This is an error"));
+    }
+
+    void shouldHaveConsoleText(JenkinsRule j, WorkflowRun run, String nodeId, String expectedText) throws IOException {
+        try (var c = j.createWebClient()) {
+            WebRequest req =
+                    new WebRequest(UrlUtils.toUrlSafe(j.getURL() + run.getUrl() + "stages/log?nodeId=" + nodeId));
+            WebResponse rsp = c.loadWebResponse(req);
+            assertThat(rsp.getContentAsString(), equalTo(expectedText));
+        }
+    }
+
+    @Issue("GH#947")
+    @Test
+    void getConsoleTextWithScriptError(JenkinsRule j) throws Exception {
+        WorkflowRun run = TestUtils.createAndRunJob(
+                j, "gh947_stage_mixed_result", "gh947_stage_mixed_result.jenkinsfile", Result.FAILURE);
+
+        PipelineNodeGraphAdapter builder = new PipelineNodeGraphAdapter(run);
+        String stageId = TestUtils.getNodesByDisplayName(run, "A").get(0).getId();
+        List<FlowNodeWrapper> stepNodes = builder.getStageSteps(stageId);
+        assertThat(stepNodes.size(), equalTo(3));
+
+        shouldHaveConsoleText(j, run, stageId, "Hello world\nThis error is caught\nThis is a fatal error\n");
+        shouldHaveConsoleText(j, run, stepNodes.get(0).getId(), "Hello world\n");
+        shouldHaveConsoleText(j, run, stepNodes.get(1).getId(), "This error is caught");
+        shouldHaveConsoleText(j, run, stepNodes.get(2).getId(), "This is a fatal error");
     }
 
     @Issue("GH#224")
